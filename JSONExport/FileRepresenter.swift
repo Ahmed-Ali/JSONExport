465d12ac4db229d7ffb2f82bcd69e2ecd830285c
Muhammad Umer <mumer92@gmail.com>
2017-12-26 11:29:29 +0500

    added back credit file

--- JSONExport/FileRepresenter.swift
+++ JSONExport/FileRepresenter.swift
@@ -189,7 +189,7 @@ class FileRepresenter{
             fileContent += ". All rights reserved.\n"
         }
         
-       // fileContent += "//\tModel file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport"
+        fileContent += "//\tModel file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport"
         
         if let langAuthor = lang.author{
             fileContent += "\n\n//\tThe \"\(lang.displayLangName!)\" support has been made available by \(langAuthor.name!)"
