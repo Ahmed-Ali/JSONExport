db67803b6e50592b35a1517fa47f76c866153294
huangjia <xyhuangjia@gmail.com>
2019-03-15 23:48:42 +0800

    fix bug click docker no reopen app

--- JSONExport/AppDelegate.swift
+++ JSONExport/AppDelegate.swift
@@ -34,17 +34,22 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if (!flag){
+           NSApp.activate(ignoringOtherApps: false)
+            NSApplication.shared.windows.first?.makeKeyAndOrderFront(self)
+        }
+        return true
+    }
 }
 
